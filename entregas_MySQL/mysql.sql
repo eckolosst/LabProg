2b43c286dd0f3a0b97c2617f9df1801c0e0fd4f9
eckolosst <lucasp.1606@gmail.com>
2018-11-26 10:16:48 -0300

    update dllmysql

--- entregas_MySQL/mysql.sql
+++ entregas_MySQL/mysql.sql
@@ -1,64 +1,62 @@
 CREATE TABLE Persona(
-tipo_doc varchar(3),
-nro_doc varchar(10),
-nombre varchar(20),
-apellido varchar(20),
-fecha_nacimiento DATE,
-estado_civil varchar(15) CHECK (estado_civil IN ('casado','soltero','comprometido')),
-domicilio varchar(30),
-email varchar(50),
-profesion varchar(20),
-empresa varchar(20),
-telefono int,
-PRIMARY KEY(tipo_doc, nro_doc));
+  tipo_doc varchar(3),
+  nro_doc varchar(10),
+  nombre varchar(20),
+  apellido varchar(20),
+  fecha_nacimiento DATE,
+  estado_civil varchar(15) CHECK (estado_civil IN ('casado','soltero','comprometido')),
+  domicilio varchar(30),
+  email varchar(50),
+  profesion varchar(20),
+  empresa varchar(20),
+  telefono int,
+  PRIMARY KEY(tipo_doc, nro_doc));
 
 CREATE TABLE Socio(
-id_socio int PRIMARY KEY AUTO_INCREMENT,
-tipo_doc varchar(3),
-nro_doc varchar(10),
-numero_socio int,
-FOREIGN KEY (tipo_doc,nro_doc) REFERENCES Persona(tipo_doc,nro_doc) on update cascade on delete cascade);
+  id_socio int PRIMARY KEY AUTO_INCREMENT,
+  tipo_doc varchar(3),
+  nro_doc varchar(10),
+  numero_socio int,
+  FOREIGN KEY (tipo_doc,nro_doc) REFERENCES Persona(tipo_doc,nro_doc) on update cascade on delete cascade);
 
 CREATE TABLE Garante(
-id_garante int PRIMARY KEY AUTO_INCREMENT,
-tipo_doc varchar(3),
-nro_doc varchar(10),
-FOREIGN KEY (tipo_doc,nro_doc) REFERENCES Persona(tipo_doc,nro_doc) on update cascade on delete cascade);
+  id_garante int PRIMARY KEY AUTO_INCREMENT,
+  tipo_doc varchar(3),
+  nro_doc varchar(10),
+  FOREIGN KEY (tipo_doc,nro_doc) REFERENCES Persona(tipo_doc,nro_doc) on update cascade on delete cascade);
 
 CREATE TABLE Periodo_afiliacion(
-id_socio int,
-id_periodo int AUTO_INCREMENT,
-fecha_alta DATE,
-fecha_baja DATE,
-PRIMARY KEY(id_socio,id_periodo),
-FOREIGN KEY (id_socio) REFERENCES Socio(id_socio) on update cascade on delete restrict);
+  id_socio int,
+  id_periodo int AUTO_INCREMENT,
+  fecha_alta DATE,
+  fecha_baja DATE,
+  PRIMARY KEY(id_socio,id_periodo),
+  FOREIGN KEY (id_socio) REFERENCES Socio(id_socio) on update cascade on delete restrict);
 
 CREATE TABLE Solicitud_prestamo(
-id_solicitud int PRIMARY KEY AUTO_INCREMENT,
-id_garante int,
-id_socio int,
-fecha_peticion DATE,
-fecha_decision DATE,
-resultado BIT,
-monto FLOAT,
-FOREIGN KEY (id_socio) REFERENCES Socio(id_socio) on update cascade on delete restrict,
-FOREIGN KEY (id_garante) REFERENCES Garante(id_garante) on update cascade on delete restrict);
+  id_solicitud int PRIMARY KEY AUTO_INCREMENT,
+  id_garante int,
+  id_socio int,
+  fecha DATE,
+  resultado BIT,
+  monto FLOAT,
+  FOREIGN KEY (id_socio) REFERENCES Socio(id_socio) on update cascade on delete restrict,
+  FOREIGN KEY (id_garante) REFERENCES Garante(id_garante) on update cascade on delete restrict);
+
+CREATE TABLE Tabla_de_referencia(
+  id_tabla_referencia int PRIMARY KEY AUTO_INCREMENT,
+  fecha_vigencia DATE,
+  monto_max FLOAT,
+  interes_por_mora FLOAT);
 
 CREATE TABLE Prestamo(
   id_prestamo int PRIMARY KEY AUTO_INCREMENT,
   id_solicitud int NOT NULL,
+  id_tabla_referencia int NOT NULL,
   tasaInteres FLOAT,
-  ingreso_mensual FLOAT,
   monto int,
   FOREIGN KEY (id_solicitud) REFERENCES Solicitud_prestamo(id_solicitud) on update cascade on delete restrict);
-
-CREATE TABLE Tabla_de_referencia(
-  id_tabla_referencia int PRIMARY KEY AUTO_INCREMENT,
-  id_prestamo int NOT NULL,
-  fecha_vigencia DATE,
-  monto_max FLOAT,
-  interes_por_mora FLOAT,
-  FOREIGN KEY (id_prestamo) REFERENCES Prestamo(id_prestamo) on update cascade on delete restrict);
+  FOREIGN KEY (id_tabla_referencia) REFERENCES Tabla_de_referencia(id_tabla_referencia) on update cascade on delete restrict);
 
 CREATE TABLE Recibo_de_sueldo(
   id_recibo int PRIMARY KEY AUTO_INCREMENT,
