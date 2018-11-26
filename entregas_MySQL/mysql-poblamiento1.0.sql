bbcedf3659e23f54889f22f0fb34aadea6fe7702
eckolosst <lucasp.1606@gmail.com>
2018-11-26 09:46:31 -0300

    poblamiento sqmy

---
+++ entregas_MySQL/mysql-poblamiento1.0.sql
@@ -0,0 +1,214 @@
+-- POBLAMIENTO PERSONAS
+insert into Persona
+    (nombre, apellido, fecha_nacimiento, estado_civil, tipo_doc, nroDocumento, domicilio, email, profesion, empresa, telefono)
+values
+    ('Joe', 'Breheny', '1990-06-16', 'soltero', 'DNI', '98871944', '370 Lindbergh Alley', 'jbreheny0@discovery.com', 'Assistant Professor', 'Viva', 4227365),
+    ('Shaughn', 'Lawdham', '1990-06-03', 'casado', 'DNI', '19851659', '085 Sachtjen Pass', 'slawdham1@cyberchimps.com', 'Marketing Manager', 'Kazu', 1713321),
+    ('Olia', 'Mearing', '1990-01-23', 'viudo', 'DNI', '3091746', '88 Waywood Road', 'omearing2@deliciousdays.com', 'Media Manager I', 'Jaxspan', 7082413),
+    ('Joleen', 'Mewett', '1990-06-23', 'casado', 'DNI', '90874350', '4900 Farwell Crossing', 'jmewett4@comsenz.com', 'Product Engineer', 'Jabberstorm', 1557602),
+    ('Alfredo', 'Ismead', '1985-10-06', 'casado', 'LC', '9320052', '890 Oneill Street', 'aismead5@msn.com', 'Senior Developer', 'Pixonyx', 2801332),
+    ('Onfroi', 'Goomes', '1985-12-28', 'divorciado', 'DNI', '50049189', '507 Farmco Trail', 'ogoomes7@ocn.ne.jp', 'Legal Assistant', 'Yabox', 4731948),
+    ('Gisela', 'Pena', '1990-05-16', 'viudo', 'DNI', '87395519', '4616 Holy Cross Circle', 'gpenab@wsj.com', 'Developer I', 'Feedspan', 8143928),
+    ('Ethelda', 'Pedgrift', '1990-03-19', 'casado', 'DNI', '80575331', '685 Mosinee Plaza', 'epedgrift9@time.com', 'Analyst Programmer', 'Babbleblab', 4611880),
+    ('Freda', 'Rodda', '1990-06-27', 'casado', 'CI', '17038863', '184 Steensland Lane', 'froddac@over-blog.com', 'Developer III', 'Tagchat', 6537234),
+    ('Jamil', 'Babon', '1990-06-23', 'divorciado', 'DNI', '08215670', '919 1st Plaza', 'jbabond@ezinearticles.com', 'Recruiter', 'Roomm', 9033389),
+    ('Blisse', 'Pavic', '1990-07-09', 'soltero', 'DNI', '8813671', '2463 4th Drive', 'bpavice@cbc.ca', 'Nurse Practicioner', 'Kazio', 4127288),
+    ('Keelby', 'Tabram', '1990-06-01', 'divorciado', 'LE', '96663754', '5136 Sommers Lane', 'ktabramf@histats.com', 'Editor', 'Muxo', 6743388),
+    ('Kamila', 'Adame', '1990-01-21', 'soltero', 'LE', '68898722', '75 Lotheville Street', 'kadameg@e-recht24.de', 'Marketing Assistant', 'Zoomlounge', 1496394),
+    ('Earvin', 'Brislane', '1990-06-16', 'casado', 'DNI', '6771624', '87 Hooker Park', 'ebrislaneh@hhs.gov', 'Geological Engineer', 'Blogpad', 1679922),
+    ('Drona', 'Boyles', '1990-01-22', 'casado', 'DNI', '79518296', '21328 Luster Trail', 'dboylesj@a8.net', 'Project Manager', 'Avamba', 8474060),
+    ('Rance', 'Venmore', '1990-07-31', 'viudo', 'DNI', '7393572', '69611 Packers Center', 'rvenmorel@51.la', 'Statistician I', 'Browsezoom', 8601258),
+    ('Lincoln', 'O''Currigan', '1985-10-17', 'soltero', 'DNI', '84403694', '69 Hintze Trail', 'locurriganq@dagondesign.com', 'Geologist I', 'Oyondu', 4642526),
+    ('Aarika', 'Bearham', '1990-07-14', 'divorciado', 'LC', '9178545', '2228 Spaight Park', 'abearhams@nymag.com', 'Software Engineer II', 'Einti', 2027456),
+    ('Erhard', 'Boatright', '1985-12-03', 'casado', 'LC', '50529818', '65 Moulton Place', 'eboatrightt@cbsnews.com', 'Account Coordinator', 'Brightdog', 2564897),
+    ('Nikita', 'Deere', '1990-05-29', 'divorciado', 'LC', '9080611', '669 Elgar Point', 'ndeerev@gmpg.org', 'Food Chemist', 'Kwinu', 2707794),
+    ('Bartram', 'Kobera', '1990-02-11', 'soltero', 'LC', '61739229', '69266 Homewood Court', 'bkoberaw@illinois.edu', 'Media Manager I', 'Roombo', 6556902),
+    ('Giuditta', 'Trafford', '1990-05-09', 'divorciado', 'DNI', '45767384', '78 Havey Way', 'gtraffordx@wisc.edu', 'Financial Analyst', 'Dynabox', 5027421),
+    ('Ira', 'Luckcock', '1990-03-02', 'soltero', 'CI', '7534392', '15925 Hallows Junction', 'iluckcockz@mozilla.org', 'VP Sales', 'Jabbercube', 1492996),
+    ('Domenic', 'Hanlin', '1985-11-24', 'soltero', 'DNI', '8699497', '681 Weeping Birch Alley', 'dhanlin10@wikipedia.org', 'Legal Assistant', 'Thoughtmix', 4823311),
+    ('Celestine', 'Blezard', '1985-09-25', 'casado', 'DNI', '26284838', '4829 Northridge Place', 'cblezard11@umich.edu', 'Engineer III', 'Kwideo', 2475683),
+    ('Perl', 'Biasetti', '1985-12-23', 'divorciado', 'DNI', '11719555', '51669 Commercial Road', 'pbiasetti12@purevolume.com', 'Graphic Designer', 'Topicware', 6544489),
+    ('Henryetta', 'Johl', '1990-05-23', 'divorciado', 'DNI', '77355891', '46 Oxford Circle', 'hjohl13@arstechnica.com', 'Staff Scientist', 'Bubblebox', 7004795),
+    ('Carley', 'Renoden', '1990-03-19', 'casado', 'DNI', '6356311', '88434 Morningstar Road', 'crenoden14@histats.com', 'Design Engineer', 'Minyx', 4045397),
+    ('Ange', 'Davidofski', '1990-03-15', 'soltero', 'DNI', '55427252', '970 Morning Court', 'adavidofski15@narod.ru', 'Assistant Manager', 'Janyx', 6804163),
+    ('Farlee', 'McNicol', '1985-12-09', 'viudo', 'DNI', '37165761', '186 Northport Crossing', 'fmcnicol16@toplist.cz', 'Director of Sales', 'Yadel', 4276018),
+    ('Rosabelle', 'Test', '1990-07-15', 'divorciado', 'LE', '74980888', '2289 Brentwood Parkway', 'rtest19@ezinearticles.com', 'Recruiting Manager', 'Realmix', 8309569),
+    ('Anette', 'Glinde', '1990-03-16', 'soltero', 'DNI', '3010363', '2587 5th Drive', 'aglinde1a@reverbnation.com', 'Graphic Designer', 'Realcube', 2497436),
+    ('Antonius', 'Spradbery', '1985-11-12', 'divorciado', 'DNI', '7523337', '786 Mallory Street', 'aspradbery1d@liveinternet.ru', 'Geologist I', 'Jaxspan', 4995168);
+
+-- POBLAMIENTO GARANTES
+insert into Garante
+    ( tipo_doc, nro_doc)
+values
+    ('DNI', '8813671'),
+    ('DNI', '6771624'),
+    ('DNI', '79518296'),
+    ('DNI', '7393572'),
+    ('DNI', '84403694'),
+    ('DNI', '50049189'),
+    ('DNI', '87395519'),
+    ('DNI', '80575331'),
+    ('CI', '17038863'),
+    ('DNI', '08215670');
+
+-- POBLAMIENTO SOCIOS
+insert into Socio (tipo_doc, nro_doc, numero_socio)
+    values
+        ('DNI', '98871944', 123),
+        ('DNI', '19851659', 124),
+        ('DNI', '3091746', 125),
+        ('DNI', '90874350', 126),
+        ('LC', '9320052', 127),
+        ('DNI', '7523337', 128),
+        ('DNI', '3010363', 129),
+        ('DNI', '37165761', 130),
+        ('DNI', '55427252', 131),
+        ('DNI', '6356311', 132),
+        ('DNI', '77355891', 133);
+
+-- POBLAMIENTO CUOTAS SOCIO
+
+insert into Cuota
+    ( capital, interes,vencimiento)
+values
+    (900, 10,'2018-12-10'),
+    (900, 10,'2019-01-10'),
+    (900, 10,'2019-02-10'),
+    (900, 10,'2019-03-10'),
+    (900, 10,'2019-04-10'),
+    (900, 10,'2019-05-10'),
+    (900, 10,'2019-06-10'),
+    (900, 10,'2019-07-10'),
+    (900, 10,'2019-08-10'),
+    (900, 10,'2019-09-10'),
+    (900, 10,'2019-10-10'),
+    (900, 10,'2019-11-10'),
+    (1200, 10,'2019-01-10'),
+    (1200, 10,'2019-02-10'),
+    (1200, 10,'2019-03-10'),
+    (1200, 10,'2019-04-10'),
+    (1200, 10,'2019-05-10'),
+    (1200, 10,'2019-06-10'),
+    (1200, 10,'2019-07-10'),
+    (1200, 10,'2019-08-10'),
+    (1200, 10,'2019-09-10'),
+    (1200, 10,'2019-10-10')
+       ;
+insert into Cuota_socio
+    (id_cuota, id_socio)
+values
+    (1, 4),
+    (2, 4),
+    (3, 4),
+    (4, 4),
+    (5, 4),
+    (6, 4),
+    (7, 4),
+    (8, 4),
+    (9, 4),
+    (10, 4),
+    (11, 4),
+    (12, 4)    ;
+
+-- POBLAMIENTO PERIODO AFILIACION
+insert into Periodo_afiliacion  ( id_socio,fecha_alta)
+values
+    (1, '1999-01-01'),
+    (2, '2001-11-10'),
+    (3, '2002-01-01'),
+    (4, '2003-01-01'),
+    (5, '2005-01-01'),
+    (6, '2006-01-01'),
+    (7, '2008-01-01'),
+    (8, '2009-01-01'),
+    (9, '2010-01-01'),
+    (10, '2012-01-01'),
+    (11, '2018-01-01');
+
+-- POBLAMIENTO EFECTIVO
+
+insert into Efectivo
+    ( id_cuota, monto,fecha)
+values
+    (1, 12000,'2018-10-13'),
+    (2, 12000,'2018-10-12'),
+    (3, 12000,'2018-10-11');
+
+-- POBLAMIENTO TARJETA
+
+insert into Tarjeta(id_cuota,monto,fecha,nro_tarjeta,banco,titular)
+values
+  (4,900,'2022-12-10','123455','BPN','ROBERTO OLMEDO'),
+  (5,900,'2022-12-10','98765','BPN','MARCELLO DINATALE'),
+  (6,900,'2022-12-10','1029384','BPN','SIMONE CORNU');
+
+-- POBLAMIENTO TRANSFERENCIA
+insert into Transferencia(id_cuota,monto,fecha,cuenta_origen,nro_transaccion)
+values
+  (7,900,'2018-11-19',123,321),
+  (8,900,'2018-11-21',1231,3211),
+  (9,900,'2018-11-01',1232,3212);
+
+insert into Solicitud_prestamo
+    (    id_garante ,    id_socio ,    fecha ,    resultado ,    monto    )
+values
+    (1, 1, '2018-01-01', '1', 100),
+    (2, 2, '2018-03-01', '1', 200),
+    (3, 3, '2018-02-01', '1', 300),
+    (4, 4, '2018-04-01', '1', 400),
+    (5, 5, '2018-05-01', '1', 500),
+    (6, 6, '2018-06-01', '1', 600),
+    (7, 7, '2018-10-01', '1', 700);
+
+insert into Recibo_de_sueldo (id_solicitud,id_garante,fecha,monto) values
+  (1,1,'2018-10-01',15000),
+  (1,1,'2018-09-01',15000),
+  (1,1,'2018-08-01',15000),
+  (2,2,'2018-08-01',15000),
+  (2,2,'2018-09-01',15000),
+  (2,2,'2018-10-01',15000);
+insert into Recibo_de_sueldo (id_solicitud,id_socio,fecha,monto) values
+  (1,1,'2018-10-01',99000),
+  (1,1,'2018-09-01',99000),
+  (1,1,'2018-08-01',99000),
+  (2,2,'2018-08-01',99000),
+  (2,2,'2018-09-01',99000),
+  (2,2,'2018-10-01',99000);
+
+insert into Tabla_de_referencia
+    ( fecha_vigencia , monto_max , interes_por_mora )
+values
+    ('2018-12-01', 1000000.00, 15.00),
+    ('2018-09-01', 10000.00, 10.00),
+    ('2018-10-01', 100000.00, 12.00);
+
+
+insert into Prestamo (id_solicitud,id_tabla_referencia,fecha,tasaInteres,monto)
+values
+(1,1,'2018-11-19',10,100),
+(2,2,'2018-10-19',12,600),
+(3,3,'2018-09-19',14,700),
+(4,1,'2018-08-19',15,800),
+(5,2,'2018-12-19',25,900);
+
+insert into Cuota_prestamo
+    (id_cuota, id_prestamo)
+values
+    (13, 1),
+    (14, 1),
+    (15, 1),
+    (16, 1),
+    (17, 1),
+    (18, 1),
+    (19, 1),
+    (20, 1),
+    (21, 1),
+    (22, 1);
+
+INSERT INTO Sorteo
+( id_socio, fecha_sorteo)
+VALUES
+( 1, '2018-01-01'),
+( 2, '2018-02-01'),
+( 3, '2018-03-01'),
+( 4, '2018-04-01');
